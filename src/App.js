a8f79f7838d7009b0401e320095a395802ce92ba
Always Abhinav <abhinav.shrivastava@gmail.com>
2025-01-01 01:03:59 +0530

    Added Profile Page

--- src/App.js
+++ src/App.js
@@ -6,14 +6,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import ProfilePage from "./components/ProfilePage"; // Example component
 
+
 const App = () => {
+  const user = {
+    name: 'John Doe',
+    about: 'A software developer with a passion for clean code.',
+    profileImage: 'https://res.cloudinary.com/dgvyblmwc/image/upload/v1563801291/qw6bkissy0s86tpxaskl.jpg'
+  };
+
+ 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/profile" element={<ProfilePage user={user} />} />
         </Routes>
       </Router>
     </ThemeProvider>
