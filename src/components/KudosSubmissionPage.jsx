0026db0b67ac5e2c31b9359e1e7cfbad9266efe5
Always Abhinav <abhinav.shrivastava@gmail.com>
2025-01-07 07:25:26 +0530

    Made changed to kudos submission by making it mandatory and profile page has the kudos date

--- src/components/KudosSubmissionPage.jsx
+++ src/components/KudosSubmissionPage.jsx
@@ -5,15 +5,18 @@ import axios from "axios";
 import { MenuItem, Select, InputLabel, FormControl } from "@mui/material";
 import { useEffect } from "react";
 import NavBar from "./NavBar";
+import { useAuth } from "./AuthContext";
 
-const KudosSubmissionPage = ({ onSubmit, userId }) => {
+const KudosSubmissionPage = () => {
+  const { user } = useAuth();
   const [text, setText] = useState("");
   const [image, setImage] = useState(null);
-  const [sender, setSender] = useState("Sam Altman");
+  // const [sender, setSender] = useState(user.displayName);
   const [receiverId, setReceiverId] = useState("");
-  const [giverId, setGiverId] = useState(userId);
-  const [loggedInUserId, setLoggedInUserId] = useState(userId);// TODO: Change this to logged in user later. Currently Ravi Patel hardcoded
-  const [users, setUsers] = useState([]);
+  const [giverId, setGiverId] = useState(user.uid);
+  // const [loggedInUserId, setLoggedInUserId] = useState(userId);// TODO: Change this to logged in user later. Currently Ravi Patel hardcoded
+  const [usersList, setUsersList] = useState([]);
+  
 
   const handleImageUpload = (event) => {
     setImage(event.target.files[0]);
@@ -24,20 +27,12 @@ const KudosSubmissionPage = ({ onSubmit, userId }) => {
     const formData = new FormData();
     formData.append("text", text);
     if (image) {
+      console.log("Image:", image);
       formData.append("image", image);
     }
-    formData.append("sender", sender);
+    // formData.append("sender", sender);
     formData.append("receiverId", receiverId);
     formData.append("giverId", giverId);
-
-        // Debugging: Log the formData values
-        // console.log("Form Data:", {
-        //   text,
-        //   image,
-        //   sender,
-        //   receiverId,
-        //   giverId,
-        // });
 
     try {
       await axios.post("/kudos", formData, {
@@ -60,71 +55,76 @@ const KudosSubmissionPage = ({ onSubmit, userId }) => {
 useEffect(() => {
   const fetchUsers = async () => {
     try {
-      const response = await axios.get(`/users?userId=${loggedInUserId}`);
-      setUsers(response.data);
+      // console.log("Giver ID", user.uid);
+      const response = await axios.get(`/users?userId=${giverId}`);
+      console.log("Users List", response.data);
+      setUsersList(response.data);
     } catch (error) {
       console.error("Error fetching users:", error);
     }
   };
 
   fetchUsers();
-}, [loggedInUserId]);
+}, [giverId]);
 
 
   return (
-<div>
-    <NavBar />
-    <Box
-      component="form"
-      onSubmit={handleSubmit}
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        gap: 2,
-        padding: "16px",
-        backgroundColor: "#ffffff",
-      }}
-    >
-      <FormControl fullWidth>
-        <InputLabel id="giver-select-label">Select Giver</InputLabel>
-        <Select
-          labelId="giver-select-label"
-          value={receiverId}
-          label="Select Giver"
-          onChange={(e) => setReceiverId(e.target.value)}
+    <div>
+      <NavBar />
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+          padding: "16px",
+          backgroundColor: "#ffffff",
+        }}
+      >
+        <FormControl fullWidth>
+          <InputLabel id="giver-select-label">Select Giver</InputLabel>
+          <Select
+            labelId="giver-select-label"
+            value={receiverId}
+            label="Select Giver"
+            onChange={(e) => setReceiverId(e.target.value)}
+            required
+          >
+            {usersList.map((user) => (
+              <MenuItem key={user.id} value={user.id}>
+                {user.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <TextField
+          label="Write a Kudo"
+          multiline
+          rows={4}
+          variant="outlined"
+          fullWidth
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          required
+        />
+        {/* TODO: Uncomment this once the image saving is setup in firebase storage in node.js */}
+        {/* <Button
+          variant="contained"
+          component="label"
+          style={{ backgroundColor: "#e60023", color: "#ffffff" }}
         >
-          {users.map((user) => (
-            <MenuItem key={user.id} value={user.id}>
-              {user.name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <TextField
-        label="Write a Kudo"
-        multiline
-        rows={4}
-        variant="outlined"
-        fullWidth
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <Button
-        variant="contained"
-        component="label"
-        style={{ backgroundColor: "#e60023", color: "#ffffff" }}
-      >
-        Upload Image
-        <input type="file" hidden onChange={handleImageUpload} />
-      </Button>
-      <Button
-        type="submit"
-        variant="contained"
-        style={{ backgroundColor: "#e60023", color: "#ffffff" }}
-      >
-        Submit
-      </Button>
-    </Box>
+          Upload Image
+          <input type="file" hidden onChange={handleImageUpload} />
+        </Button> */}
+        <Button
+          type="submit"
+          variant="contained"
+          style={{ backgroundColor: "#e60023", color: "#ffffff" }}
+        >
+          Submit
+        </Button>
+      </Box>
     </div>
   );
 };
--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -12,8 +12,8 @@ const ProfilePage = () => {
   useEffect(() => {
     const fetchProfileData = async () => {
       try {
-        console.log("User Object:", user);
-        console.log("User ID:", user.uid);
+        // console.log("User Object:", user);
+        // console.log("User ID:", user.uid);
         const response = await axios.get(`/profile?userId=${user.uid}`);
         // console.log("Response:", response);
         setProfileData(response.data.user);
@@ -32,65 +32,91 @@ const ProfilePage = () => {
   // console.log("User Below:", user);
 
   return (
-  <div>
-    <NavBar />
-    <div style={{ padding: "16px", backgroundColor: "#ffffff" }}>
-      {/* User Info */}
-      <div style={{ display: "flex", alignItems: "center", marginBottom: "16px" }}>
-        <Avatar
-          alt={profileData.name}
-          src={profileData.profileImage}
-          style={{
-            width: 120,
-            height: 120,
-            border: "2px solid #e60023",
-            marginRight: "16px",
-            borderRadius: 0, // Set borderRadius to 0 to make it a square
-          }}
-        />
-        <div>
-          <Typography variant="h4" style={{ fontWeight: "bold", color: "#333333" }}>
-            {profileData.name}
+    <div>
+      <NavBar />
+      <div style={{ display: "flex", flexDirection: "column", padding: "16px", backgroundColor: "#ffffff" }}>
+        {/* Profile Section */}
+        <div style={{ backgroundColor: "#f0f0f0", padding: "16px", marginBottom: "8px" }}>
+          <div style={{ display: "flex", alignItems: "center", marginBottom: "16px" }}>
+            <Avatar
+              alt={profileData.name}
+              src={profileData.profileImage}
+              style={{
+                width: 120,
+                height: 120,
+                border: "2px solid #e60023",
+                marginRight: "16px",
+                borderRadius: 0,
+              }}
+            />
+            <div>
+              <Typography variant="h4" style={{ fontWeight: "bold", color: "#333333" }}>
+                {profileData.name}
+              </Typography>
+              <Typography variant="body1" color="textSecondary" style={{ color: "#717171" }}>
+                {profileData.bio}
+              </Typography>
+            </div>
+          </div>
+        </div>
+        {/* Tags Section */}
+        <div style={{ backgroundColor: "#d0d0d0", padding: "16px" }}>
+          <Typography variant="h6" style={{ fontWeight: "bold", color: "#333333" }}>
+            Tags
           </Typography>
-          <Typography variant="body1" color="textSecondary" style={{ color: "#717171" }}>
-            {profileData.bio}
-          </Typography>
+          <Box display="flex" flexWrap="wrap" gap={1} mt={2}>
+          {["Team Player", "Hardworking", "Innovative", "Reliable"].map((tag, index) => (
+            <Box
+            key={index}
+            sx={{
+              backgroundColor: "#ffffff",
+              color: "#333333",
+              padding: "8px 16px",
+              borderRadius: "16px",
+              boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+            }}
+            >
+            {tag}
+            </Box>
+          ))}
+          </Box>
+        </div>        
+        {/* Kudos Section */}
+        <div style={{ backgroundColor: "#e0e0e0", padding: "16px", marginBottom: "8px" }}>
+          <Grid container spacing={2}>
+            {kudos.map((kudo) => (
+              <Grid item xs={12} sm={6} md={4} key={kudo.id}>
+                <Card>
+                  <CardContent>
+                    <Typography variant="body1" style={{ color: "#333333" }}>
+                      {kudo.text}
+                    </Typography>
+                  </CardContent>
+                  <Box display="flex" alignItems="center">
+                    <CardMedia
+                      component="img"
+                      image={kudo.giver.image}
+                      alt={kudo.giver.name}
+                      sx={{ width: 40, height: 40, borderRadius: '50%', marginLeft: 2 }}
+                    />
+                    <Typography variant="body2" sx={{ color: "#717171", marginRight: 'auto' }}>
+                      {kudo.giver.name}
+                    </Typography>
+                    <Typography variant="body2" sx={{ color: "#717171", marginLeft: 'auto' }}>
+                      {new Date(kudo.createdDate).toLocaleDateString('en-GB', {
+                        day: '2-digit',
+                        month: 'short',
+                        year: 'numeric'
+                      }).replace(/ /g, '-')}
+                    </Typography>
+                  </Box>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
         </div>
       </div>
-      {/* Kudos List */}
-      <Grid container spacing={2}>
-        {kudos.map((kudo) => (
-          <Grid item xs={12} sm={6} md={4} key={kudo.id}>
-            <Card>
-            <CardContent>
-                <Typography variant="body1" style={{ color: "#333333" }}>
-                  {kudo.text}
-                </Typography>
-              </CardContent>              
-              <Box display="flex" alignItems="center">
-                <CardMedia
-                  component="img"
-                  image={kudo.giver.image}
-                  alt={kudo.giver.name}
-                  sx={{ width: 40, height: 40, borderRadius: '50%', marginLeft: 2 }}
-                />
-                <Typography variant="body2" sx={{ color: "#717171",  marginRight: 'auto' }}>
-                  {kudo.giver.name}
-                </Typography>
-                <Typography variant="body2" sx={{ color: "#717171",  marginLeft: 'auto' }}>
-                  {new Date(kudo.createdDate).toLocaleDateString('en-GB', {
-                    day: '2-digit',
-                    month: 'short',
-                    year: 'numeric'
-                  }).replace(/ /g, '-')}
-                </Typography>
-              </Box>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
     </div>
-  </div>
   );
 };
 
